1b8b740306a757c996f3acb992dadeb52bbf8c1d
ev15963 <ev15963@hs.ac.kr>
2020-06-08 16:22:30 +0900

    reduce(); 6월 8일 14시

--- src/bibilog/arrayMethod.js
+++ src/bibilog/arrayMethod.js
@@ -71,3 +71,20 @@ var arr2 = arr.find(function (n) {
   return n % 5 == 0;
 });
 console.log(arr2); //undefined
+
+// 배열 요소의 합 계산하기
+var arr = [9, 2, 8, 5, 7];
+var sum = arr.reduce(function (pre, value) {
+  return pre + value;
+});
+console.log(sum);
+
+// 배열 요소의 합 계산하기
+var arr = [9, 2, 8, 5, 7];
+var count = 0;
+var sum = arr.reduce(function (pre, value) {
+  count++;
+  return pre + value;
+}, 0); // initialValue가 주어졌다!
+console.log(sum); // 31
+console.log(count); // 5
