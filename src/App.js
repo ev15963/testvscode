0f7ac2e8d7a7130777ae0c0cb9bba87823dec771
ev15963 <ev15963@hs.ac.kr>
2020-06-12 23:58:10 +0900

    6월 11일 03-2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+//작성한 컴포넌트 소환
+import TodaysPlan from './03/TodaysPlan';
 
 class App extends React.Component {
   render() {
@@ -7,8 +9,11 @@ class App extends React.Component {
       <div>
         <img src="http://www.easyspub.co.kr/images/logo_footer.png" />
         <div>안녕하세요</div>
+        <TodaysPlan />
       </div>
     );
   }
 }
 export default App;
+
+//JSX안에 마크업형식으로 추가
