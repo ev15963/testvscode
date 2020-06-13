5f2ee3f066b7f1c40d5b105cb7e2d4bf720ca28c
ev15963 <ev15963@hs.ac.kr>
2020-06-13 17:16:07 +0900

    불리언프로퍼티 사용하기 ~17:15

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import React from 'react';
 //작성한 컴포넌트 소환
 // import TodaysPlan from './03/TodaysPlan';
 // import PropsComponent from './03/PropsCompnent';
-import ChildComponent from './03/ChildComponent';
+// import ChildComponent from './03/ChildComponent';
+import BooleanComponent from './03/BooleanComponent';
 
 class App extends React.Component {
   render() {
-    // const array = [1,2,3];
-    // const obj = {name: '제목', age: 30};
-    // const node = <h1>노드</h1>;
-    // const func = () =>
     return (
-      <ChildComponent
-        boolValue={true} // 불리언 값 전달
-        numValue={1} //숫자를 전달
-        arrayValue={[1, 2, 3]} //배열 전달
-        objValue={{ name: '제목', age: 30 }} // 객체 전달
-        nodeValue={<h1>노드</h1>} //노드 전달
-        funcValue={() => {
-          //함수 객체를 전달
-          console.log('메시지');
-        }}
-      />
+      <div>
+        <div>
+          <b>지루할 때 : </b> {/*이름만 전달하기*/}
+          <BooleanComponent bored />
+        </div>
+        <div>
+          <b>즐거울 때 : </b> {/** 이름 전달하지 않을때 */}
+          <BooleanComponent />
+        </div>
+      </div>
     );
   }
 }
