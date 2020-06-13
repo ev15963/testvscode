5c7957643dba6713de5393b11e41abe4231f33a0
ev15963 <ev15963@hs.ac.kr>
2020-06-14 04:26:42 +0900

    ChildProperty 04:26

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import React from 'react';
 // import ChildComponent from './03/ChildComponent';
 // import BooleanComponent from './03/BooleanComponent';
 // import ChildComponent2 from './03/ChildComponent2';
-import DefaultPropsComponent from './03/DefaultPropsComponent';
+// import DefaultPropsComponent from './03/DefaultPropsComponent';
+import ChildProperty from './03/ChildProperty';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <DefaultPropsComponent />
+        <ChildProperty>
+          <div>
+            <span>자식노드</span>
+          </div>
+        </ChildProperty>
       </div>
     );
   }
