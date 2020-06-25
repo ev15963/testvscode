50c700b3f78bb9c7891567b65412913fefa80474
ev15963 <ev15963@hs.ac.kr>
2020-06-25 16:01:45 +0900

    Counter 컴퍼넌트 작성 16:01

--- src/03/Counter.jsx
+++ src/03/Counter.jsx
@@ -5,7 +5,7 @@ class Counter extends React.Component {
     super(props);
     //state 정의
     this.state = {
-      count: 0,
+      count: props.count,
     };
     //이후 콜백 함수를 다룰 때 bind(this)에 대하여
     this.increaseCount = this.increaseCount.bind(this);
@@ -20,8 +20,7 @@ class Counter extends React.Component {
   render() {
     return (
       <div>
-        {/* state 데이터는 this.state로 접근 가능합니다. */}
-        <span>카운트: {this.state.count}</span>
+        현재 카운트: {this.state.count}
         <button onClick={this.increaseCount}>카운트증가</button>
       </div>
     );
