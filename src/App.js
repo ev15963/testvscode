512f4f2ccdffd00cbb2bc406f4b8bf3626ceae1c
ev15963 <ev15963@hs.ac.kr>
2020-06-25 15:51:01 +0900

    소멸과정의 생명주기 함수들 실행해 보기 15:50

--- src/App.js
+++ src/App.js
@@ -13,12 +13,15 @@ import React from 'react';
 import LifecycleExample from './03/LifecycleExample';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasDestroyed: false };
+  }
+  componentDidMount() {
+    this.setState({ hasDestroyed: true });
+  }
   render() {
-    return (
-      <div>
-        <LifecycleExample />
-      </div>
-    );
+    return <div>{this.state.hasDestroyed ? null : <LifecycleExample />}</div>;
   }
 }
 export default App;
