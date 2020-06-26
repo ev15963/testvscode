f537d77b8b09e310f0aaba279164e5689f712c59
ev15963 <ev15963@hs.ac.kr>
2020-06-26 17:41:01 +0900

    내부 호이스팅 17:40

---
+++ src/exercise/exercise.js
@@ -0,0 +1,14 @@
+/** --- JS Parser 내부의 호이스팅(Hoisting)의 결과 - 위와 동일 --- */
+var foo2; // [Hoisting] 함수표현식의 변수값 "선언"
+
+function foo() {
+  // [Hoisting] 함수선언문
+  console.log('hello');
+}
+
+foo();
+//foo2(); // ERROR!!
+
+foo2 = function () {
+  console.log('hello2');
+};
