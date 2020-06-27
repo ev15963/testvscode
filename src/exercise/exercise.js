19fdad6cf6736ea2bc617a93ad16b26cec26e373
ev15963 <ev15963@hs.ac.kr>
2020-06-27 23:59:31 +0900

    6/27 함수선언문vs함수표현식

--- src/exercise/exercise.js
+++ src/exercise/exercise.js
@@ -1,3 +1,15 @@
+foo();
+foo2();
+
+function foo() {
+  // 함수선언문
+  console.log('hello');
+}
+var foo2 = function () {
+  // 함수표현식
+  console.log('hello2');
+};
+
 /** --- JS Parser 내부의 호이스팅(Hoisting)의 결과 - 위와 동일 --- */
 var foo2; // [Hoisting] 함수표현식의 변수값 "선언"
 
