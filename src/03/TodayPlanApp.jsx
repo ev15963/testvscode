bbe732ed14704c2bf40e370ee39b7c3d094ec013
ev15963 <ev15963@hs.ac.kr>
2020-06-29 03:56:15 +0900

    Component ~ Todolist 03:51

---
+++ src/03/TodayPlanApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TodaysPlanApp extends React.Component {
+  render() {
+    const { onButtonClick, hasPlan } = this.props;
+    return (
+      <div className="body">
+        {hasPlan ? <TodaysPlan /> : null}
+        <button onClick={onButtonClick}>계획없음</button>
+      </div>
+    );
+  }
+}
