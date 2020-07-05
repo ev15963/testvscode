2d6a2e7a5df823dee6c73153bc753f396439918a
ev15963 <ev15963@hs.ac.kr>
2020-07-05 20:28:53 +0900

    컴포넌트에서 DOM 이벤트 사용하기 20:28

---
+++ src/03/Counter3.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Counter3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+    this.increateCount = this.increateCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
+  }
+  increateCount() {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  }
+  resetCount() {
+    this.setState({ count: 0 });
+  }
+  render() {
+    return (
+      <div>
+        현재 카운트 {this.props.count}
+        <button
+          onClick={this.this.increateCount} //버튼이 클릭될 떄 카운트를 증가하는 함수를 호출합니다.
+          onMouseOut={this.resetCount}
+        >
+          {' '}
+          {/**마우스가 버튼 밖으로 이동하면 카운트를 초기화하는 함수를 호출합니다. */}
+          카운트증가
+        </button>
+        버튼 밖으로 커서가 움직이면 0으로 초기화됩니다.
+      </div>
+    );
+  }
+}
--- src/03/ScrollSpy.jsx
+++ src/03/ScrollSpy.jsx
@@ -5,14 +5,15 @@ export default class ScrollSpy extends React.PureComponent {
     super(props);
     this.setRef = this.setRef.bind(this);
     this.checkPosition = this.checkPosition.bind(this);
-    window.addEventListener('scroll', this.checkPosition);
+    window.addEventListener('scroll', this.checkPosition); //윈도우의 스크롤 이동 이벤트를 감지하여 checkPostion() 함수가 실행되도록 하였습니다.
   }
   setRef(ref) {
-    this.ref = ref;
+    this.ref = ref; //ref 프로퍼티에서 반환된 객체를 컴포넌트 변수에 할당되었습니다.
   }
 
   checkPosition() {
     if (this.ref.getBoundClientRect().top < window.innerHeight) {
+      //현재 DOM 객체의 위치가 스크롤 화면 안팎인지 측정하고 있습니다.
       console.log('enter');
     } else {
       console.log('exit');
@@ -20,7 +21,7 @@ export default class ScrollSpy extends React.PureComponent {
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.checkPosition);
+    window.removeEventListener('scroll', this.checkPosition); //컴포넌트가 소멸할 때 화면 스크롤 감시 함수를 메모리에서 제거하였습니다.
   }
 
   render() {
