917efb202b96d963890b907b959f5a58c53492f1
ev15963 <ev15963@hs.ac.kr>
2020-07-06 03:12:53 +0900

    Input 컴포넌트 만들면서 복습 03:12

---
+++ src/03/Input.jsx
@@ -0,0 +1,70 @@
+import React, { pureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends PureComponent {
+  //
+  constructor(props) {
+    super(props);
+    this.setRef = this.setRef.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    const { name, onChange } = this.props;
+    if (onChange) {
+      onChange(name, e.target.value);
+    }
+  }
+  componentDidMount() {
+    if (this.props.autoFocus) {
+      // 클래스형 컴포넌트의 PureComponent 형태로 작성
+      this.ref.focus();
+    }
+  }
+  componentDidUpdate() {
+    if (this.props.autoFocus) {
+      // autoFocus값이 true일 경우 출력 후 input 박스에 자동으로 커서를 배치
+      this.ref.focus();
+    }
+  }
+  setRef(ref) {
+    this.ref = ref;
+  }
+
+  render() {
+    const { errorMessage, label, name, value, type, onFocus } = this.props;
+    return (
+      <label>
+        {label} {/**autoFocus값이 true일 경우 출력 후 input 박스에 자동으로 커서를 배치 */}
+        <input
+          id={'input_${name}'}
+          ref={this.setRef}
+          onChange={this.handleChange} /**label 안에 전달받은 label겂을 출력 */
+          onFocus={onFocus} /**input 값이 변경될 때 onChange 콜백 함수를 호출*/
+          value={value}
+          type={type}
+        />
+        {errorMessage && <span className="error">{errorMessage}</span>}{' '}
+        {/**프로퍼티로 전달받은 콜백 함수를 DOM 이벤트 프로퍼티에 연결하여 input에 마우스 커서가 포커스될 때 상위 컴포넌트의 콜백 함수를 호출*/}
+      </label>
+    );
+  }
+}
+
+Input.propTypes = {
+  type: PropTypes.oneOf(['text', 'number', 'price']), // 문자열형의 값 'text', 'number', 'price' 중 하나의 값만 가질 수 있음
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  errorMessage: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  onFocus: PropTypes.func,
+  autoFocus: PropTypes.bool,
+};
+Input.defaultProps = {
+  onChange: () => {},
+  onFocus: () => {},
+  autoFocus: false,
+  type: 'text', // type의 기본값은 'text'입니다.
+};
+
+export default Input;
