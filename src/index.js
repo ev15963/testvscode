198e5a5bfe53797d6faa7066bb5db43797919398
ev15963 <ev15963@hs.ac.kr>
2020-07-12 17:34:59 +0900

    스토리에 다른 형태의 컴포넌트 추가하기 17:34

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+export default from '../storybook';
+
 //리액트 엔진이 화면을 출력하는 코드
 //id가 root인 엘리먼트에 컴포넌트를 출력
 ReactDOM.render(<App />, document.getElementById('root'));
