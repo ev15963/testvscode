73c7dd2f40b3e4218103c06de3014d5404ac99ec
ev15963 <ev15963@hs.ac.kr>
2020-10-11 23:49:29 +0900

    storybook에서 머티러얼 디자인 구현 해보기 23:49

--- src/InputStory.jsx
+++ src/InputStory.jsx
@@ -2,5 +2,12 @@ import React from 'react';
 import { stroiesOf } from '@storybook.react';
 
 import Input from '../03/Input';
+import { action } from '@storybook/addon-actions';
 
-stroiesOf('Input, module').add('기본 설정', () => <Input />);
+stroiesOf('Input, module')
+  .add('기본 설정', () => <Input />)
+  .addWithJSX('기본설정', () => <Input name="123"></Input>)
+  .addWithJSX('label 예제', () => <Input name="123" label="이름"></Input>)
+  .addWithJSX('onChange 에제', () => (
+    <Input name="123" label="이름" onChange={action('onChange Event Occurs')}></Input>
+  ));
