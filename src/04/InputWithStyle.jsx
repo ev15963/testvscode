58dacb3718bd259ced6477a7457df7e9d47e1c1b
ev15963 <ev15963@hs.ac.kr>
2020-10-18 17:58:04 +0900

    material 오류 17:57

--- src/04/InputWithStyle.jsx
+++ src/04/InputWithStyle.jsx
@@ -31,21 +31,23 @@ class Input extends PureComponent {
   }
 
   render() {
-    const { errorMessage, label, name, value, type, onFocus } = this.props;
+    const { errorMessage, label, name, value, type } = this.props;
     return (
-      <label>
-        {label} {/**autoFocus값이 true일 경우 출력 후 input 박스에 자동으로 커서를 배치 */}
+      <div className="input-field">
         <input
           id={`input_${name}`}
+          className="validate"
           ref={this.setRef}
           onChange={this.handleChange} /**label 안에 전달받은 label겂을 출력 */
-          onFocus={onFocus} /**input 값이 변경될 때 onChange 콜백 함수를 호출*/
           value={value}
           type={type}
         />
-        {errorMessage && <span className="error">{errorMessage}</span>}
+        <label htmlFor={`input_${name}`}>
+          {label}
+        </label>
+        {errorMessage && <span className="helper-text">{errorMessage}</span>}
         {/**프로퍼티로 전달받은 콜백 함수를 DOM 이벤트 프로퍼티에 연결하여 input에 마우스 커서가 포커스될 때 상위 컴포넌트의 콜백 함수를 호출*/}
-      </label>
+        </div>
     );
   }
 }
